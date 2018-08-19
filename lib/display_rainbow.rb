fda1c894a9c26e5bc640d729a29cd1a4563f425b
Alan Perez <alanp9six@gmail.com>
2018-08-19 04:21:49 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,6 +5,6 @@ puts "R: #{display_rainbow[0]}", "O: #{display_rainbow[1]}", "Y: #{display_rainb
 
 def print_colors(letter)
 
-    puts "R: #{display_rainbow[0]}", "O: #{display_rainbow[1]}", "Y: #{display_rainbow[2]}", "G: #{display_rainbow[3]}", "B: #{display_rainbow[4]}", "I: #{display_rainbow[5]}", "V: #{display_rainbow[6]}"
+    puts "R: #{letter[0]}", "O: #{letter[1]}", "Y: #{letter[2]}", "G: #{letter[3]}", "B: #{letter[4]}", "I: #{letter[5]}", "V: #{letter[6]}"
 
 end
